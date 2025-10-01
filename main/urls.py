502dab7dd5c4a016f531d77d3f96433df641dd5a
ganeshatq <ganeshataqwa01@gmail.com>
2025-10-01 10:01:00 +0700

    Tugas Individu Kelima

--- main/urls.py
+++ main/urls.py
@@ -1,20 +1,19 @@
 from django.urls import path
-from main.views import show_main, create_product, show_product, show_xml, show_json, show_json_by_id, show_xml_by_id
-from main.views import register, login_user, logout_user
-
+from main.views import show_main, create_products, show_products, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_products, delete_products
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('create-product/', create_product, name= 'create_product'),
-    path('product/<str:id>/', show_product, name='show_product'),
+    path('create-products/', create_products, name='create_products'),
+    path('products/<str:id>/', show_products, name='show_products'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-    path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/<str:products_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:products_id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-
+    path('products/<int:id>/edit', edit_products, name='edit_products'),
+    path('products/<int:id>/delete', delete_products, name='delete_products'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,91 +1,101 @@
 import datetime
-from django.urls import reverse
-
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import ProductForm
+from main.models import Product
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-from django.shortcuts import render, redirect, get_object_or_404
-from main.forms import ProductForm
-from main.models import Product
-
-# Create your views here.
-
 @login_required(login_url='/login')
 def show_main(request):
+    filter_type = request.GET.get('filter', 'all')
 
-    filter_type = request.GET.get("filter", "all")
-
-    if filter_type == "all":
-        product_list = Product.objects.all()
+    if (filter_type == 'all'):
+        products_list = Product.objects.all()
     else:
-        product_list = Product.objects.filter(user=request.user)
-
+        products_list = Product.objects.filter(user=request.user)
     context = {
-        'nama_aplikasi' : 'Toko Olahraga BeliYuk',
-        'npm' : '2406425640',
-        'nama': 'Ganesha Taqwa',
-        'kelas': 'PBP F',
-        'product_list': product_list,
+        'appname' : 'Toko Olahraga BeliYuk',
+        'name': 'Ganesha Taqwa',
+        'class': 'PBP F',
+        'products_list': products_list,
         'last_login': request.COOKIES.get('last_login', 'Never'),
-        'current_user': request.COOKIES.get('current_user', 'Anonymous')
     }
 
     return render(request, "main.html", context)
 
-def create_product(request):
+@login_required(login_url='/login')
+def create_products(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        product_entry = form.save(commit=False)
-        product_entry.user = request.user
-        product_entry.save()
+        products_entry = form.save(commit=False)
+        products_entry.user = request.user
+        products_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
-    return render(request, "create_product.html", context)
+    return render(request, "create_products.html", context)
 
 @login_required(login_url='/login')
-def show_product(request, id):
-    product = get_object_or_404(Product, pk=id)
+def show_products(request, id):
+    products = get_object_or_404(Product, pk=id)
+    products.increment_views()
 
     context = {
-        'product': product
+        'products': products
     }
 
-    return render(request, "product_detail.html", context)
+    return render(request, "products_detail.html", context)
+
+def edit_products(request, id):
+    products = get_object_or_404(Product, pk=id)
+    form = ProductForm(request.POST or None, instance=products)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_products.html", context)
+
+def delete_products(request, id):
+    products = get_object_or_404(Product, pk=id)
+    products.delete()
+    return HttpResponseRedirect(reverse("main:show_main"))
 
 def show_xml(request):
-    product_list = Product.objects.all()
-    xml_data = serializers.serialize("xml", product_list)
+    products_list = Product.objects.all()
+    xml_data = serializers.serialize("xml", products_list)
     return HttpResponse(xml_data, content_type="application/xml")
 
 def show_json(request):
-    product_list = Product.objects.all()
-    json_data = serializers.serialize("json", product_list)
+    products_list = Product.objects.all()
+    json_data = serializers.serialize("json", products_list)
     return HttpResponse(json_data, content_type="application/json")
 
-def show_xml_by_id(request, product_id):
-    try:
-        product_item = Product.objects.filter(pk=product_id)
-        xml_data = serializers.serialize("xml", product_item)
+def show_xml_by_id(request, products_id):
+   try:
+        products_item = Product.objects.filter(pk=products_id)
+        xml_data = serializers.serialize("xml", products_item)
         return HttpResponse(xml_data, content_type="application/xml")
-    except Product.DoesNotExist:
-       return HttpResponse(status=404)
-    
-
-def show_json_by_id(request, product_id):
-    try:
-        product_item = Product.objects.filter(pk=product_id)
-        json_data = serializers.serialize("json", product_item)
-        return HttpResponse(json_data, content_type="application/json")
-    except Product.DoesNotExist:
+   except Product.DoesNotExist:
         return HttpResponse(status=404)
-
-
+   
+def show_json_by_id(request, products_id):
+   try:
+        products_item = Product.objects.get(pk=products_id)
+        json_data = serializers.serialize("json", [products_item])
+        return HttpResponse(json_data, content_type="application/json")
+   except Product.DoesNotExist:
+        return HttpResponse(status=404)
+   
 def register(request):
     form = UserCreationForm()
 
@@ -99,25 +109,23 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
-            response.set_cookie('current_user', user.username)
             return response
 
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
-    response.delete_cookie('current_user')
     return response
